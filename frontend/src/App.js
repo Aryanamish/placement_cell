21e4be89e5b47e3432602c84c00c883226ce2377
Vivek2024 <94036493+Vivek2024@users.noreply.github.com>
2022-07-10 23:20:27 +0530

    navabar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import ButtonSubmit from "./Components/Basics/ButtonSubmit";
 import Input from "./Components/Basics/Input";
 import Password from "./Components/Basics/Password";
 import NavBar from './Components/NavBar';
+import { Folder } from "./Components/Folder";
 import LoginForm from "./Components/LoginForm";
 
 function App() {
   return (
     <>
       <NavBar/>
-      <LoginForm/> 
+      <Folder/>
+      {/* <LoginForm/> */}
     </>
   );
 }
